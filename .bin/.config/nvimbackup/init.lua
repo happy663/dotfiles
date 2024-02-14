6e3e0f8f9d4563dcb62c022f8feff92393f8bbee
happy663 <tatu2425@gmail.com>
2024-02-15 01:09:44 +0900

    update: gitignoreの調整

--- .bin/.config/nvimbackup/init.lua
+++
@@ -1,8 +0,0 @@
-require'plugins'
-require("options")
-require('lualine').setup()
-
-vim.cmd("colorscheme nightfox")
-
--- vim.cmd("colorscheme kanagawa")
--- vim.cmd[[autocmd BufWritePost plugins.lua PackerCompile]]
