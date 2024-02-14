8c1affbe0bdb574229727158278ce29662d15557
happy663 <tatu2425@gmail.com>
2024-02-15 01:49:22 +0900

    update: シンボリックリンクスクリプト更新

--- .bin/link.sh
+++ .bin/link.sh
@@ -3,6 +3,8 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 for dotfile in "${SCRIPT_DIR}"/.??* ; do
     [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
+    [[ "$dotfile" == "${SCRIPT_DIR}/.github" ]] && continue
+    [[ "$dotfile" == "${SCRIPT_DIR}/.DS_Store" ]] && continue
 
     ln -fnsv "$dotfile" "$HOME"
 done
