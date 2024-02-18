0aafab65282255531a5dc60a7fc269582d56679f
happy663 <tatu2425@gmail.com>
2024-02-19 05:36:52 +0900

    add: bufを開いた時にtree上が開くように

--- .bin/.config/nvim/lua/auto_command.lua
+++ .bin/.config/nvim/lua/auto_command.lua
@@ -64,3 +64,13 @@ vim.api.nvim_create_autocmd("CursorMoved", {
     vim.cmd("normal! zz")
   end,
 })
+
+local function auto_update_path()
+  local buf = vim.api.nvim_get_current_buf()
+  local bufname = vim.api.nvim_buf_get_name(buf)
+  if vim.fn.isdirectory(bufname) or vim.fn.isfile(bufname) then
+    require("nvim-tree.api").tree.find_file(vim.fn.expand("%:p"))
+  end
+end
+
+vim.api.nvim_create_autocmd("BufEnter", { callback = auto_update_path })
