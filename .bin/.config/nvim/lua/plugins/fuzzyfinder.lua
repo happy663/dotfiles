cb31806431df8561a8c6c3341fa3d79af58efe3b
happy663 <tatu2425@gmail.com>
2024-02-23 03:53:27 +0900

    refacotor: コメント

--- .bin/.config/nvim/lua/plugins/fuzzyfinder.lua
+++ .bin/.config/nvim/lua/plugins/fuzzyfinder.lua
@@ -20,10 +20,10 @@ return {
         pickers = {},
         extensions = {
           fzf = {
-            fuzzy = true,             -- false will only do exact Matching
-            override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true, -- override the file sorter
-            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
           },
         },
       })
--- .bin/.config/nvim/lua/plugins/git.lua
+++ .bin/.config/nvim/lua/plugins/git.lua
@@ -5,7 +5,6 @@ return {
   },
   {
     "kdheepak/lazygit.nvim",
-    -- optional for floating window border decoration
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
