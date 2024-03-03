fbe1d4aceb5c013878b7eaa75628e61a8ed251e0
happy663 <tatu2425@gmail.com>
2024-03-03 12:45:21 +0900

    refactor: コメント消去

--- .bin/.config/nvim/lua/plugins/ui.lua
+++ .bin/.config/nvim/lua/plugins/ui.lua
@@ -1,229 +1,206 @@
 return {
-	{
-		"nvim-lualine/lualine.nvim",
-		config = function()
-			require("lualine").setup({
-				options = {
-					theme = "ayu_mirage",
-				},
-			})
-		end,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-			opt = true,
-		},
-	},
-	{
-		"nvim-tree/nvim-tree.lua",
-		config = function()
-			require("nvim-tree").setup({
-				sort_by = "case_sensitive",
-				view = {
-					width = 30,
-				},
-				renderer = {
-					group_empty = true,
-				},
-				filters = {
-					dotfiles = false,
-				},
-			})
-		end,
-		dependencies = "nvim-tree/nvim-web-devicons",
-	},
-	{
-		"akinsho/bufferline.nvim",
-		dependencies = "kyazdani42/nvim-web-devicons",
-		version = "*",
-		config = function()
-			local bufferline = require("bufferline")
-			bufferline.setup({
-				options = {
-					numbers = "none",
-					diagnostics = "nvim_lsp",
-					diagnostics_update_in_insert = false,
-					diagnostics_indicator = function(count, level, diagnostics_dict, context)
-						local icon = level:match("error") and " " or " "
-						return " " .. icon .. count
-					end,
-					style_preset = bufferline.style_preset.no_italic,
-				},
-			})
-		end,
-	},
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		config = true,
-	},
-	{
-		"folke/noice.nvim",
-		config = function()
-			require("noice").setup({
-				view = {
-					mini = {
-						position = "bottom-right",
-					},
-				},
-				lsp = {
-					override = {
-						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-						["vim.lsp.util.stylize_markdown"] = true,
-						["cmp.entry.get_documentation"] = true,
-					},
-					hover = {
-						enabled = false,
-					},
-				},
-				routes = {
-					{
-						filter = {
-							event = "msg_show", -- msg_showイベントのメッセージを対象
-							min_height = 1, -- 最小高さが1行のメッセージを対象
-						},
-						view = "mini", -- 上で定義したカスタムビューにルーティング
-					},
-					{
-						filter = {
-							event = "notify",
-							find = "No information available",
-						},
-						opts = { skip = true },
-					},
-					{
-						filter = {
-							event = "notify",
-							find = "method textDocument/hover is not supported by any of the servers registered for the current buffer",
-						},
-						opts = { skip = true },
-					},
-					{
-						filter = {
-							event = "notify",
-							find = "# Config Change Detected. Reloading...",
-						},
-					},
-				},
-				presets = {
-					bottom_search = false,
-					command_palette = true,
-					long_message_to_split = true,
-					inc_rename = false,
-					lsp_doc_border = false,
-				},
-			})
-		end,
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
-		},
-	},
-	{
-		"akinsho/toggleterm.nvim",
-		version = "*",
-		config = function()
-			require("toggleterm").setup({
-				size = 20,
-				open_mapping = [[<c-\>]],
-				hide_numbers = true,
-				shade_filetypes = {},
-				shade_terminals = true,
-				shading_factor = 2,
-				start_in_insert = true,
-				insert_mappings = true,
-				persist_size = true,
-				direction = "float",
-				float_opts = {
-					border = "curved",
-					winblend = 0,
-					highlights = {
-						border = "Normal",
-						background = "Normal",
-					},
-				},
-			})
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = function()
-			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-			ts_update()
-		end,
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = "all",
-				sync_install = false,
-				auto_install = false,
-				ignore_install = {},
-				autotag = {
-					enable = true,
-				},
-				highlight = {
-					enable = true,
-					disable = function()
-						return vim.g.vscode
-					end,
-				},
-			})
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		dependencies = "nvim-treesitter/nvim-treesitter",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				textobjects = {
-					select = {
-						enable = true,
-
-						-- Automatically jump forward to textobj, similar to targets.vim
-						lookahead = true,
-
-						keymaps = {
-							-- You can use the capture groups defined in textobjects.scm
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							-- You can optionally set descriptions to the mappings (used in the desc parameter of
-							-- nvim_buf_set_keymap) which plugins like which-key display
-							["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-							-- You can also use captures from other query groups like `locals.scm`
-							["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-						},
-						-- You can choose the select mode (default is charwise 'v')
-						--
-						-- Can also be a function which gets passed a table with the keys
-						-- * query_string: eg '@function.inner'
-						-- * method: eg 'v' or 'o'
-						-- and should return the mode ('v', 'V', or '<c-v>') or a table
-						-- mapping query_strings to modes.
-						selection_modes = {
-							["@parameter.outer"] = "v", -- charwise
-							["@function.outer"] = "V", -- linewise
-							["@class.outer"] = "<c-v>", -- blockwise
-						},
-						-- If you set this to `true` (default is `false`) then any textobject is
-						-- extended to include preceding or succeeding whitespace. Succeeding
-						-- whitespace has priority in order to act similarly to eg the built-in
-						-- `ap`.
-						--
-						-- Can also be a function which gets passed a table with the keys
-						-- * query_string: eg '@function.inner'
-						-- * selection_mode: eg 'v'
-						-- and should return true of false
-						include_surrounding_whitespace = true,
-					},
-				},
-			})
-		end,
-	},
-	{
-		"antosha417/nvim-lsp-file-operations",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-tree.lua",
-		},
-		config = true,
-	},
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "ayu_mirage",
+        },
+      })
+    end,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      opt = true,
+    },
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+      require("nvim-tree").setup({
+        sort_by = "case_sensitive",
+        view = {
+          width = 30,
+        },
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = false,
+        },
+      })
+    end,
+    dependencies = "nvim-tree/nvim-web-devicons",
+  },
+  {
+    "akinsho/bufferline.nvim",
+    dependencies = "kyazdani42/nvim-web-devicons",
+    version = "*",
+    config = function()
+      local bufferline = require("bufferline")
+      bufferline.setup({
+        options = {
+          numbers = "none",
+          diagnostics = "nvim_lsp",
+          diagnostics_update_in_insert = false,
+          diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            local icon = level:match("error") and " " or " "
+            return " " .. icon .. count
+          end,
+          style_preset = bufferline.style_preset.no_italic,
+        },
+      })
+    end,
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    config = true,
+  },
+  {
+    "folke/noice.nvim",
+    config = function()
+      require("noice").setup({
+        view = {
+          mini = {
+            position = "bottom-right",
+          },
+        },
+        lsp = {
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+          hover = {
+            enabled = false,
+          },
+        },
+        routes = {
+          {
+            filter = {
+              event = "msg_show", -- msg_showイベントのメッセージを対象
+              min_height = 1, -- 最小高さが1行のメッセージを対象
+            },
+            view = "mini", -- 上で定義したカスタムビューにルーティング
+          },
+          {
+            filter = {
+              event = "notify",
+              find = "No information available",
+            },
+            opts = { skip = true },
+          },
+          {
+            filter = {
+              event = "notify",
+              find = "method textDocument/hover is not supported by any of the servers registered for the current buffer",
+            },
+            opts = { skip = true },
+          },
+          {
+            filter = {
+              event = "notify",
+              find = "# Config Change Detected. Reloading...",
+            },
+          },
+        },
+        presets = {
+          bottom_search = false,
+          command_palette = true,
+          long_message_to_split = true,
+          inc_rename = false,
+          lsp_doc_border = false,
+        },
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        size = 20,
+        open_mapping = [[<c-\>]],
+        hide_numbers = true,
+        shade_filetypes = {},
+        shade_terminals = true,
+        shading_factor = 2,
+        start_in_insert = true,
+        insert_mappings = true,
+        persist_size = true,
+        direction = "float",
+        float_opts = {
+          border = "curved",
+          winblend = 0,
+          highlights = {
+            border = "Normal",
+            background = "Normal",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+      local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+      ts_update()
+    end,
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = "all",
+        sync_install = false,
+        auto_install = false,
+        ignore_install = {},
+        autotag = {
+          enable = true,
+        },
+        highlight = {
+          enable = true,
+          disable = function()
+            return vim.g.vscode
+          end,
+        },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+              ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+            },
+            selection_modes = {
+              ["@parameter.outer"] = "v", -- charwise
+              ["@function.outer"] = "V", -- linewise
+              ["@class.outer"] = "<c-v>", -- blockwise
+            },
+            include_surrounding_whitespace = true,
+          },
+        },
+      })
+    end,
+  },
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-tree.lua",
+    },
+    config = true,
+  },
 }
