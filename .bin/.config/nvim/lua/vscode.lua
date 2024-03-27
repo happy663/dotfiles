c72cb8ed9b475e26d8b85eeb8190657f1907c6bf
happy663 <tatu2425@gmail.com>
2024-03-28 00:32:31 +0900

    add: vscode-neovim-config

--- .bin/.config/nvim/lua/vscode.lua
+++ .bin/.config/nvim/lua/vscode.lua
@@ -4,3 +4,17 @@ local opts = { noremap = true, silent = true }
 map("n", "<leader>/", "<Plug>NERDCommenterToggle", opts)
 map("v", "<leader>/", "<Plug>NERDCommenterToggle", opts)
 map("n", "<Leader>f", "<CMD>FuzzyMotion<CR>", opts)
+map("n", "gp", '"*p', opts)
+map("n", "gP", '"*P', opts)
+map("n", "<CR>", "A<Return><Esc>k", opts)
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("TextYankPost", {
+  callback = function()
+    if vim.v.event.operator == "y" and vim.v.event.regname == "" then
+      vim.fn.setreg("*", vim.fn.getreg('"'))
+      vim.fn.setreg("+", vim.fn.getreg('"'))
+    end
+  end,
+})
