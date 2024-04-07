d17060d17bc074ce2ddd6d0a7349aeed7c2cc352
happy663 <tatu2425@gmail.com>
2024-04-07 15:40:50 +0900

    add:copilot chatを導入する

--- .bin/.config/nvim/lua/plugins/tools.lua
+++ .bin/.config/nvim/lua/plugins/tools.lua
@@ -44,4 +44,19 @@ return {
       vim.fn["mkdp#util#install"]()
     end,
   },
+  {
+    {
+      "CopilotC-Nvim/CopilotChat.nvim",
+      branch = "canary",
+      dependencies = {
+        { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+        { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+      },
+      opts = {
+        debug = true, -- Enable debugging
+        -- See Configuration section for rest
+      },
+      -- See Commands section for default commands if you want to lazy load on them
+    },
+  },
 }
