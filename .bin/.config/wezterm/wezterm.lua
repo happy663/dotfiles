9258b51d09568f39d9433e1f702ebbc71d2249d9
happy663 <tatu2425@gmail.com>
2024-04-07 15:39:27 +0900

    update: windowsとmacでweztermの設定をわける

--- .bin/.config/wezterm/wezterm.lua
+++ .bin/.config/wezterm/wezterm.lua
@@ -1,30 +1,41 @@
+-- Pull in the wezterm API
 local wezterm = require("wezterm")
 
-local config = {}
+-- This will hold the configuration.
+local config = wezterm.config_builder()
 
-if wezterm.config_builder then
-  config = wezterm.config_builder()
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "AdventureTime"
+wezterm.log_info(wezterm.target_triple)
+
+-- windows
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+  config.window_background_image = "C:/Users/toyama/wallpaperflare.com_wallpaper.jpg"
+  config.default_domain = "WSL:Ubuntu-22.04"
 end
 
--- カラースキームの設定
-config.color_scheme = "AdventureTime"
+-- mac
+if wezterm.target_triple == "x86_64-apple-darwin" then
+  config.window_background_image =
+    "/Users/toyama/src/github.com/dotfiles/.bin/.config/wezterm/images/wallpaperflare.com_wallpaper.jpg"
+end
+
+-- 最初からフルスクリーンで起動
+local mux = wezterm.mux
+wezterm.on("gui-startup", function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():toggle_fullscreen()
+end)
 
 -- 背景透過
 config.window_background_opacity = 1
-
 config.initial_cols = 120
 config.initial_rows = 60
-
-config.window_background_image =
-  "/Users/toyama/src/github.com/dotfiles/.bin/.config/wezterm/images/wallpaperflare.com_wallpaper.jpg"
-
 config.window_background_image_hsb = {
-  -- Darken the background image by reducing it to 1/3rd
   brightness = 0.2,
-  -- You can adjust the hue by scaling its value.
-  -- a multiplier of 1.0 leaves the value unchanged.
   hue = 1,
-  -- You can adjust the saturation also.
   saturation = 0.9,
 }
 
@@ -33,9 +44,12 @@ config.inactive_pane_hsb = {
   brightness = 0.8,
 }
 
--- ショートカットキー設定
 local act = wezterm.action
 config.keys = {
+  -- paste from the clipboard
+  { key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+  -- paste from the primary selection
+  { key = "V", mods = "CTRL", action = act.PasteFrom("PrimarySelection") },
   -- Alt(Opt)+Shift+Fでフルスクリーン切り替え
   {
     key = "f",
@@ -43,5 +57,5 @@ config.keys = {
     action = wezterm.action.ToggleFullScreen,
   },
 }
-
+-- and finally, return the configuration to wezterm
 return config
