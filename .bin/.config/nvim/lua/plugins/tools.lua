ba11efa6aacd4a2503b44eb7001de53bfe7a19d5
happy663 <tatu2425@gmail.com>
2024-04-21 06:19:07 +0900

    feat: add CopilotChat setup in tools.lua

--- .bin/.config/nvim/lua/plugins/tools.lua
+++ .bin/.config/nvim/lua/plugins/tools.lua
@@ -53,6 +53,79 @@ return {
         -- debug = true, -- Enable debugging
         -- See Configuration section for rest
       },
+      config = function()
+        local select = require("CopilotChat.select")
+
+        require("CopilotChat").setup({
+          debug = true, -- Enable debugging
+          -- プロンプトの設定
+          -- デフォルトは英語なので日本語でオーバーライドしています
+          prompts = {
+            MyCustomPrompt = {
+              prompt = "どう機能するか教えてください",
+              mapping = "<leader>ccmc",
+              description = "My custom prompt description",
+              selection = require("CopilotChat.select").visual,
+            },
+            Explain = {
+              prompt = "/COPILOT_EXPLAIN カーソル上のコードの説明を段落をつけて書いてください。",
+            },
+            Tests = {
+              prompt = "/COPILOT_TESTS カーソル上のコードの詳細な単体テスト関数を書いてください。",
+            },
+            Fix = {
+              prompt = "/COPILOT_FIX このコードには問題があります。バグを修正したコードに書き換えてください。",
+            },
+            Optimize = {
+              prompt = "/COPILOT_REFACTOR 選択したコードを最適化し、パフォーマンスと可読性を向上させてください。",
+            },
+            Docs = {
+              prompt = "/COPILOT_REFACTOR 選択したコードのドキュメントを書いてください。ドキュメントをコメントとして追加した元のコードを含むコードブロックで回答してください。使用するプログラミング言語に最も適したドキュメントスタイルを使用してください（例：JavaScriptのJSDoc、Pythonのdocstringsなど）",
+            },
+            FixDiagnostic = {
+              prompt = "ファイル内の次のような診断上の問題を解決してください：",
+              selection = select.diagnostics,
+            },
+          },
+        })
+      end,
+      cmd = "CopilotChatMyCustomPrompt",
+      keys = {
+        {
+          "<leader>ccmc",
+        },
+        {
+          "<leader>ccq",
+          function()
+            local input = vim.fn.input("Quick Chat: ")
+            if input ~= "" then
+              require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+            end
+          end,
+          desc = "CopilotChat - Quick chat",
+        },
+        {
+          "<leader>cch",
+          function()
+            local actions = require("CopilotChat.actions")
+            require("CopilotChat.integrations.telescope").pick(actions.help_actions())
+          end,
+          desc = "CopilotChat - Help actions",
+        },
+        -- Show prompts actions with telescope
+        {
+          "<leader>ccp",
+          function()
+            local actions = require("CopilotChat.actions")
+            require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+          end,
+          desc = "CopilotChat - Prompt actions",
+          mode = {
+            "n",
+            "v",
+          },
+        },
+      },
       -- See Commands section for default commands if you want to lazy load on them
     },
   },
