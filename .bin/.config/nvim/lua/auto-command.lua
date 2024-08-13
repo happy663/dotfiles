60a63212ab92f94666ac92f02bf4e52a771affae
happy663 <tatu2425@gmail.com>
2024-08-13 11:27:30 +0900

    update: vim-jp radio更新

--- .bin/.config/nvim/lua/auto-command.lua
+++ .bin/.config/nvim/lua/auto-command.lua
@@ -44,6 +44,7 @@ local function memo_commit_async()
   local handle
   -- memo commitコマンドを非同期に実行
   handle, _ = vim.loop.spawn(
+
     "memo",
     {
       args = { "commit" },
@@ -103,6 +104,10 @@ vim.api.nvim_create_autocmd("User", {
       ["'"] = { "っ" },
       ["_"] = { "-" },
       ["："] = { ":" },
+      ["zv"] = { "←" },
+      ["zb"] = { "↓" },
+      ["zn"] = { "↑" },
+      ["zm"] = { "→" },
     })
 
     vim.api.nvim_exec(
@@ -159,7 +164,7 @@ vim.api.nvim_create_autocmd("User", {
 local play_process_id = nil
 -- 音声を再生する関数
 local function play_sound()
-  local filepath = vim.fn.expand("~/Downloads/VimJpRadio/20240715.mp3")
+  local filepath = vim.fn.expand("~/Downloads/VimJpRadio/20240805.mp3")
   play_process_id = vim.fn.jobstart('afplay "' .. filepath .. '"', { detach = true })
 end
 
