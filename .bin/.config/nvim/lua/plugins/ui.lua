cfb26ec05d9ee0178955bed39dbc7c8edf66fc9e
happy663 <tatu2425@gmail.com>
2024-09-15 06:15:01 +0900

    feat: マクロ記録時のstatusを表示

--- .bin/.config/nvim/lua/plugins/ui.lua
+++ .bin/.config/nvim/lua/plugins/ui.lua
@@ -7,6 +7,15 @@ return {
         options = {
           theme = "ayu_mirage",
         },
+        sections = {
+          lualine_x = {
+            {
+              require("noice").api.statusline.mode.get,
+              cond = require("noice").api.statusline.mode.has,
+              color = { fg = "#ff9e64" },
+            },
+          },
+        },
       })
     end,
     dependencies = {
