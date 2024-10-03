65ba0026cdd85ca675e06dcc9453def097976b0c
happy663 <tatu2425@gmail.com>
2024-10-03 13:06:32 +0900

    feat: 余計なものを消して作業できるプラグインを追加

--- .bin/.config/nvim/lua/plugins/tools.lua
+++ .bin/.config/nvim/lua/plugins/tools.lua
@@ -394,4 +394,12 @@ return {
       nn.setup({ activate_hydra_keys = "<leader>h", repl_provider = "molten" })
     end,
   },
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 }
