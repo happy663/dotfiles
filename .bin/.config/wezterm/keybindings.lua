29f3b48aa38e63f1f1f74108d93065fecc04c219
happy663 <tatu2425@gmail.com>
2024-10-03 12:51:48 +0900

    feat:wezterm

--- .bin/.config/wezterm/keybindings.lua
+++ .bin/.config/wezterm/keybindings.lua
@@ -11,6 +11,13 @@ function M.apply(config)
     { key = "U", mods = "CTRL|SHIFT", action = wezterm.action.EmitEvent("toggle-opacity-and-remove-background-image") },
     { key = "C", mods = "CTRL|SHIFT", action = wezterm.action.EmitEvent("change-window-background-image") },
     { key = "w", mods = "ALT", action = wezterm.action.HideApplication },
+    -- Ctrl+Tab をNeovimに渡す
+    { key = "Tab", mods = "CTRL", action = wezterm.action({ SendString = "\x1b[27;5;9~" }) },
+    -- Ctrl+Shift+Tab も同様に
+    { key = "Tab", mods = "CTRL|SHIFT", action = wezterm.action({ SendString = "\x1b[27;6;9~" }) },
+    -- オプション4: Command/Ctrl + [ ]
+    { key = "[", mods = "ALT", action = wezterm.action.ActivateTabRelative(-1) },
+    { key = "]", mods = "ALT", action = wezterm.action.ActivateTabRelative(1) },
   }
 
   config.macos_forward_to_ime_modifier_mask = "CTRL|SHIFT"
