37c184f7ba6fab15b4cc07f42fca43ffe095ab3c
happy663 <tatu2425@gmail.com>
2024-10-24 21:16:21 +0900

    feat: lazygit.nvimの設定を追加

--- .bin/.config/nvim/lua/plugins/git.lua
+++ .bin/.config/nvim/lua/plugins/git.lua
@@ -7,9 +7,22 @@ return {
   {
     "kdheepak/lazygit.nvim",
     cond = vim.g.not_in_vscode,
+    lazy = false,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
     dependencies = {
+      "nvim-telescope/telescope.nvim",
       "nvim-lua/plenary.nvim",
     },
+    config = function()
+      require("telescope").load_extension("lazygit")
+    end,
   },
   {
     "sindrets/diffview.nvim",
