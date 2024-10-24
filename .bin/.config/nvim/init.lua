25296df9e494d7bfea77e84b90f8976661d2a390
happy663 <tatu2425@gmail.com>
2024-10-25 02:30:22 +0900

    feat: lazygitでのコミット時に特定の行にカーソルを移動させてInsertモードにする

--- .bin/.config/nvim/init.lua
+++ .bin/.config/nvim/init.lua
@@ -44,5 +44,5 @@ else
 end
 
 if vim.fn.has("nvim") == 1 and vim.fn.executable("nvr") == 1 then
-  vim.env.GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
+  vim.env.GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe' +'normal! 7G' +'startinsert'"
 end
