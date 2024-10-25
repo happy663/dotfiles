5b86de46a7a9f60afa6a532188893f61a48a2048
happy663 <tatu2425@gmail.com>
2024-10-25 12:43:50 +0900

    feat(wezterm): add keybindings
    - リーダーキーを設定
    - タブの操作をリーダーキーに変更
    - Paneの操作を追加
    - ワークスペースの操作を追加
    - ワークスペースの名前変更を追加
    - ワークスペースの新規作成を追加
    - コピーモードを追加
    - タブの移動をリーダーキーに変更
    - クリップボードのコピーを追加
    - クリップボードのペーストを追加
    - フルスクリーンのトグルを追加
    - 設定のリロードを追加

--- .bin/.config/wezterm/keybindings.lua
+++ .bin/.config/wezterm/keybindings.lua
@@ -1,27 +1,74 @@
 -- keybindings.lua
 local wezterm = require("wezterm")
+local act = wezterm.action
 local M = {}
 
 function M.apply(config)
+  config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 2000 }
   config.keys = {
     -- ctrl+alt+tでnew tab
-    { key = "t", mods = "CTRL|SHIFT", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+    { key = "t", mods = "LEADER", action = act({ SpawnTab = "CurrentPaneDomain" }) },
     -- close
-    { key = "w", mods = "CTRL|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
-    { key = "V", mods = "CTRL", action = wezterm.action.PasteFrom("Clipboard") },
-    { key = "V", mods = "CTRL", action = wezterm.action.PasteFrom("PrimarySelection") },
-    { key = "F", mods = "CTRL|SHIFT", action = wezterm.action.ToggleFullScreen },
-    { key = "R", mods = "CTRL|SHIFT", action = wezterm.action.ReloadConfiguration },
-    { key = "U", mods = "CTRL|SHIFT", action = wezterm.action.EmitEvent("toggle-opacity-and-remove-background-image") },
-    { key = "C", mods = "CTRL|SHIFT", action = wezterm.action.EmitEvent("change-window-background-image") },
-    { key = "w", mods = "ALT", action = wezterm.action.HideApplication },
+    { key = "w", mods = "LEADER", action = act({ CloseCurrentTab = { confirm = true } }) },
+    { key = "V", mods = "SUPER", action = act.PasteFrom("Clipboard") },
+    { key = "C", mods = "SUPER", action = act.CopyTo("Clipboard") },
+    { key = "F", mods = "CTRL|SHIFT", action = act.ToggleFullScreen },
+    { key = "R", mods = "CTRL|SHIFT", action = act.ReloadConfiguration },
+    { key = "U", mods = "CTRL|SHIFT", action = act.EmitEvent("toggle-opacity-and-remove-background-image") },
+    { key = "C", mods = "CTRL|SHIFT", action = act.EmitEvent("change-window-background-image") },
+    { key = "w", mods = "ALT", action = act.HideApplication },
     -- Ctrl+Tab をNeovimに渡す
-    { key = "Tab", mods = "CTRL", action = wezterm.action({ SendString = "\x1b[27;5;9~" }) },
+    { key = "Tab", mods = "CTRL", action = act({ SendString = "\x1b[27;5;9~" }) },
     -- Ctrl+Shift+Tab も同様に
-    { key = "Tab", mods = "CTRL|SHIFT", action = wezterm.action({ SendString = "\x1b[27;6;9~" }) },
+    { key = "Tab", mods = "CTRL|SHIFT", action = act({ SendString = "\x1b[27;6;9~" }) },
     -- オプション4: Command/Ctrl + [ ]
-    { key = "[", mods = "ALT", action = wezterm.action.ActivateTabRelative(-1) },
-    { key = "]", mods = "ALT", action = wezterm.action.ActivateTabRelative(1) },
+    { key = "[", mods = "ALT", action = act.ActivateTabRelative(-1) },
+    { key = "]", mods = "ALT", action = act.ActivateTabRelative(1) },
+    -- Pane
+    { key = "r", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { key = "d", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { key = "x", mods = "LEADER", action = act({ CloseCurrentPane = { confirm = true } }) },
+    -- Pane移動 leader + hlkj
+    { key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+    { key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+    { key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+    { key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+    {
+      mods = "LEADER",
+      key = "s",
+      action = act.ShowLauncherArgs({ flags = "WORKSPACES", title = "Select workspace" }),
+    },
+    {
+      --workspaceの名前変更
+      key = "$",
+      mods = "LEADER",
+      action = act.PromptInputLine({
+        description = "(wezterm) Set workspace title:",
+        action = wezterm.action_callback(function(win, pane, line)
+          if line then
+            wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line)
+          end
+        end),
+      }),
+    },
+    {
+      key = "S",
+      mods = "LEADER|SHIFT",
+      action = act.PromptInputLine({
+        description = "(wezterm) Create new workspace:",
+        action = wezterm.action_callback(function(window, pane, line)
+          if line then
+            window:perform_aciton(
+              act.SwitchToWorkspace({
+                name = line,
+              }),
+              pane
+            )
+          end
+        end),
+      }),
+    },
+    { key = "[", mods = "LEADER", action = act.ActivateCopyMode },
   }
 
   config.macos_forward_to_ime_modifier_mask = "CTRL|SHIFT"
