f0fd27be6f0dc7b24bb7b743fe52e8f204f51ce8
happy663 <tatu2425@gmail.com>
2024-10-25 17:15:54 +0900

    feat: resize_paneのキーバインディングを追加
    - resize_paneのキーバインディングを追加
    - leader + eでresize_paneのキーバインディングを有効化する

--- .bin/.config/wezterm/keybindings.lua
+++ .bin/.config/wezterm/keybindings.lua
@@ -69,9 +69,84 @@ function M.apply(config)
       }),
     },
     { key = "[", mods = "LEADER", action = act.ActivateCopyMode },
+    { key = "e", mods = "LEADER", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
   }
 
   config.macos_forward_to_ime_modifier_mask = "CTRL|SHIFT"
+
+  config.key_tables = {
+    -- Paneサイズ調整 leader + s
+    resize_pane = {
+      { key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+      { key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+      { key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+      { key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+
+      -- Cancel the mode by pressing escape
+      { key = "Enter", action = "PopKeyTable" },
+    },
+    activate_pane = {
+      { key = "h", action = act.ActivatePaneDirection("Left") },
+      { key = "l", action = act.ActivatePaneDirection("Right") },
+      { key = "k", action = act.ActivatePaneDirection("Up") },
+      { key = "j", action = act.ActivatePaneDirection("Down") },
+    },
+    -- copyモード leader + [
+    copy_mode = {
+      -- 移動
+      { key = "h", mods = "NONE", action = act.CopyMode("MoveLeft") },
+      { key = "j", mods = "NONE", action = act.CopyMode("MoveDown") },
+      { key = "k", mods = "NONE", action = act.CopyMode("MoveUp") },
+      { key = "l", mods = "NONE", action = act.CopyMode("MoveRight") },
+      -- 最初と最後に移動
+      { key = "^", mods = "NONE", action = act.CopyMode("MoveToStartOfLineContent") },
+      { key = "$", mods = "NONE", action = act.CopyMode("MoveToEndOfLineContent") },
+      -- 左端に移動
+      { key = "0", mods = "NONE", action = act.CopyMode("MoveToStartOfLine") },
+      { key = "o", mods = "NONE", action = act.CopyMode("MoveToSelectionOtherEnd") },
+      { key = "O", mods = "NONE", action = act.CopyMode("MoveToSelectionOtherEndHoriz") },
+      --
+      { key = ";", mods = "NONE", action = act.CopyMode("JumpAgain") },
+      -- 単語ごと移動
+      { key = "w", mods = "NONE", action = act.CopyMode("MoveForwardWord") },
+      { key = "b", mods = "NONE", action = act.CopyMode("MoveBackwardWord") },
+      { key = "e", mods = "NONE", action = act.CopyMode("MoveForwardWordEnd") },
+      -- ジャンプ機能 t f
+      { key = "t", mods = "NONE", action = act.CopyMode({ JumpForward = { prev_char = true } }) },
+      { key = "f", mods = "NONE", action = act.CopyMode({ JumpForward = { prev_char = false } }) },
+      { key = "T", mods = "NONE", action = act.CopyMode({ JumpBackward = { prev_char = true } }) },
+      { key = "F", mods = "NONE", action = act.CopyMode({ JumpBackward = { prev_char = false } }) },
+      -- 一番下へ
+      { key = "G", mods = "NONE", action = act.CopyMode("MoveToScrollbackBottom") },
+      -- 一番上へ
+      { key = "g", mods = "NONE", action = act.CopyMode("MoveToScrollbackTop") },
+      -- viweport
+      { key = "H", mods = "NONE", action = act.CopyMode("MoveToViewportTop") },
+      { key = "L", mods = "NONE", action = act.CopyMode("MoveToViewportBottom") },
+      { key = "M", mods = "NONE", action = act.CopyMode("MoveToViewportMiddle") },
+      -- スクロール
+      { key = "b", mods = "CTRL", action = act.CopyMode("PageUp") },
+      { key = "f", mods = "CTRL", action = act.CopyMode("PageDown") },
+      { key = "d", mods = "CTRL", action = act.CopyMode({ MoveByPage = 0.5 }) },
+      { key = "u", mods = "CTRL", action = act.CopyMode({ MoveByPage = -0.5 }) },
+      -- 範囲選択モード
+      { key = "v", mods = "NONE", action = act.CopyMode({ SetSelectionMode = "Cell" }) },
+      { key = "v", mods = "CTRL", action = act.CopyMode({ SetSelectionMode = "Block" }) },
+      { key = "V", mods = "NONE", action = act.CopyMode({ SetSelectionMode = "Line" }) },
+      -- コピー
+      { key = "y", mods = "NONE", action = act.CopyTo("Clipboard") },
+
+      -- コピーモードを終了
+      {
+        key = "Enter",
+        mods = "NONE",
+        action = act.Multiple({ { CopyTo = "ClipboardAndPrimarySelection" }, { CopyMode = "Close" } }),
+      },
+      { key = "Escape", mods = "NONE", action = act.CopyMode("Close") },
+      { key = "c", mods = "CTRL", action = act.CopyMode("Close") },
+      { key = "q", mods = "NONE", action = act.CopyMode("Close") },
+    },
+  }
 end
 
 return M
