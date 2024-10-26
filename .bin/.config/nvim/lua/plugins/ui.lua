c3f397e978d5738349fad5590a4c4944272cc076
happy663 <tatu2425@gmail.com>
2024-10-26 15:58:30 +0900

    feat: Add lasterisk.nvim
    - Add lasterisk.nvim
    - Add keymap for lasterisk.nvim

--- .bin/.config/nvim/lua/plugins/ui.lua
+++ .bin/.config/nvim/lua/plugins/ui.lua
@@ -325,4 +325,24 @@ return {
       })
     end,
   },
+  {
+    "rapan931/lasterisk.nvim",
+    cond = vim.g.not_in_vscode,
+    config = function()
+      vim.keymap.set("n", "*", function()
+        require("lasterisk").search()
+        require("hlslens").start()
+        vim.cmd("set hlsearch")
+        vim.cmd("HlSearchLensEnable")
+        vim.g.highlight_on = true
+      end)
+      vim.keymap.set({ "n", "x" }, "g*", function()
+        require("lasterisk").search({ is_whole = false })
+        require("hlslens").start()
+        vim.cmd("set hlsearch")
+        vim.cmd("HlSearchLensEnable")
+        vim.g.highlight_on = true
+      end)
+    end,
+  },
 }
