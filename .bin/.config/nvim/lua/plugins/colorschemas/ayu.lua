f06bf4ea88577eb9f4fce9ae9b09d5a898f532c9
happy663 <tatu2425@gmail.com>
2025-02-05 19:48:01 +0900

    feat:snacks.nvimの追加

--- .bin/.config/nvim/lua/plugins/colorschemas/ayu.lua
+++ .bin/.config/nvim/lua/plugins/colorschemas/ayu.lua
@@ -5,16 +5,16 @@ return {
     config = function()
       require("ayu").setup({
         overrides = {
-          Normal = { bg = "None" },
-          ColorColumn = { bg = "None" },
-          SignColumn = { bg = "None" },
-          Folded = { bg = "None" },
-          FoldColumn = { bg = "None" },
-          CursorLine = { bg = "None" },
-          CursorColumn = { bg = "None" },
-          WhichKeyFloat = { bg = "None" },
-          VertSplit = { bg = "None" },
-          LineNr = { fg = "#7c869c" },
+          -- Normal = { bg = "None" },
+          -- ColorColumn = { bg = "None" },
+          -- SignColumn = { bg = "None" },
+          -- Folded = { bg = "None" },
+          -- FoldColumn = { bg = "None" },
+          -- CursorLine = { bg = "None" },
+          -- CursorColumn = { bg = "None" },
+          -- WhichKeyFloat = { bg = "None" },
+          -- VertSplit = { bg = "None" },
+          -- LineNr = { fg = "#7c869c" },
         },
       })
     end,
--- .bin/.config/nvim/lua/plugins/ui/nvim-tree.lua
+++ .bin/.config/nvim/lua/plugins/ui/nvim-tree.lua
@@ -1,68 +1,68 @@
 return {
-  {
-    "nvim-tree/nvim-tree.lua",
-    cond = vim.g.not_in_vscode,
-    config = function()
-      require("nvim-tree").setup({
-        sort_by = "case_sensitive",
-        view = {
-          width = {},
-        },
-        update_focused_file = {
-          enable = true,
-        },
-        renderer = {
-          group_empty = true,
-          highlight_git = true,
-          highlight_opened_files = "name",
-          icons = {
-            glyphs = {
-              git = {
-                unstaged = "!",
-                renamed = "»",
-                untracked = "?",
-                deleted = "✘",
-                staged = "✓",
-                unmerged = "",
-                ignored = "◌",
-              },
-            },
-          },
-        },
-        filters = {
-          git_ignored = true,
-          dotfiles = false,
-          custom = {
-            "__pycache__",
-            "^.git$",
-          },
-        },
-      })
-      local map = vim.api.nvim_set_keymap
-
-      local api = require("nvim-tree.api")
-      vim.keymap.set("n", "<leader>ea", function()
-        api.tree.expand_all()
-      end, { silent = true, nowait = true, desc = "Expand all" })
-
-      vim.keymap.set("n", "<leader>ec", function()
-        api.tree.collapse_all()
-      end, { silent = true, nowait = true, desc = "Collapse all" })
-
-      vim.keymap.set("n", "<leader>ei", function()
-        api.tree.toggle_gitignore_filter()
-      end, { silent = true, nowait = true, desc = "Toggle gitignore filter" })
-
-      map("n", "<C-b>", ":NvimTreeToggle<CR>", {
-        silent = true,
-        noremap = true,
-        desc = "NvimTreeToggle",
-      })
-
-      vim.api.nvim_set_var("loaded_netrw", 1)
-      vim.api.nvim_set_var("loaded_netrwPlugin", 1)
-    end,
-
-    dependencies = "nvim-tree/nvim-web-devicons",
-  },
+  -- {
+  --   "nvim-tree/nvim-tree.lua",
+  --   cond = vim.g.not_in_vscode,
+  --   config = function()
+  --     require("nvim-tree").setup({
+  --       sort_by = "case_sensitive",
+  --       view = {
+  --         width = {},
+  --       },
+  --       update_focused_file = {
+  --         enable = true,
+  --       },
+  --       renderer = {
+  --         group_empty = true,
+  --         highlight_git = true,
+  --         highlight_opened_files = "name",
+  --         icons = {
+  --           glyphs = {
+  --             git = {
+  --               unstaged = "!",
+  --               renamed = "»",
+  --               untracked = "?",
+  --               deleted = "✘",
+  --               staged = "✓",
+  --               unmerged = "",
+  --               ignored = "◌",
+  --             },
+  --           },
+  --         },
+  --       },
+  --       filters = {
+  --         git_ignored = true,
+  --         dotfiles = false,
+  --         custom = {
+  --           "__pycache__",
+  --           "^.git$",
+  --         },
+  --       },
+  --     })
+  --     local map = vim.api.nvim_set_keymap
+  --
+  --     local api = require("nvim-tree.api")
+  --     vim.keymap.set("n", "<leader>ea", function()
+  --       api.tree.expand_all()
+  --     end, { silent = true, nowait = true, desc = "Expand all" })
+  --
+  --     vim.keymap.set("n", "<leader>ec", function()
+  --       api.tree.collapse_all()
+  --     end, { silent = true, nowait = true, desc = "Collapse all" })
+  --
+  --     vim.keymap.set("n", "<leader>ei", function()
+  --       api.tree.toggle_gitignore_filter()
+  --     end, { silent = true, nowait = true, desc = "Toggle gitignore filter" })
+  --
+  --     map("n", "<C-b>", ":NvimTreeToggle<CR>", {
+  --       silent = true,
+  --       noremap = true,
+  --       desc = "NvimTreeToggle",
+  --     })
+  --
+  --     vim.api.nvim_set_var("loaded_netrw", 1)
+  --     vim.api.nvim_set_var("loaded_netrwPlugin", 1)
+  --   end,
+  --
+  --   dependencies = "nvim-tree/nvim-web-devicons",
+  -- },
 }
