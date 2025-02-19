68b9219d9dc51aaefad3ac1dec0bb76df06c7ed1
happy663 <tatu2425@gmail.com>
2025-02-19 23:29:29 +0900

    update:

---
+++ .bin/.config/nvim/lua/plugins/tools/overseer.lua
@@ -0,0 +1,8 @@
+return {
+  -- "stevearc/overseer.nvim",
+  -- keys = {
+  --   { "<space>r", "<CMD>OverseerRun<CR>" },
+  --   { "<space>R", "<CMD>OverseerToggle<CR>" },
+  -- },
+  -- opts = {},
+}
