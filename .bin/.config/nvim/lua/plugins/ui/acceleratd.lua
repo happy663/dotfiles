4a1c765632e44de8779619363acabb19b4c66307
happy663 <tatu2425@gmail.com>
2025-03-15 03:20:30 +0900

    feat: accelerated-jk.nvimを追加

---
+++ .bin/.config/nvim/lua/plugins/ui/acceleratd.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "rainbowhxch/accelerated-jk.nvim",
+    config = function()
+      vim.api.nvim_set_keymap("n", "j", "<Plug>(accelerated_jk_gj)", {})
+      vim.api.nvim_set_keymap("n", "k", "<Plug>(accelerated_jk_gk)", {})
+    end,
+  },
+}
