3dec5381c6a826a41b757410d22d4e252d75e8b2
happy663 <tatu2425@gmail.com>
2025-03-20 18:36:59 +0900

    feat: nixpkgsの更新

--- conf/.config/nix/flake.nix
+++ conf/.config/nix/flake.nix
@@ -3,21 +3,45 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, neovim-nightly-overlay }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system}.extend (
+        neovim-nightly-overlay.overlays.default
+      );
+    in
+    {
 
-    packages.aarch64-darwin.hello = nixpkgs.legacyPackages.aarch64-darwin.hello;
-    packages.aarch64-darwin.default = self.packages.aarch64-darwin.hello;
+      packages.${system}.my-package = pkgs.buildEnv {
+        name = "my-package-list";
+        paths = with pkgs;
+          [
+            hello
+            curl
+          ]
+          ++ [
+            neovim
+          ];
+      };
 
-    packages.aarch64-darwin.my-package = nixpkgs.legacyPackages.aarch64-darwin.buildEnv {
-      name = "my-package-list";
-      paths = [
-        nixpkgs.legacyPackages.aarch64-darwin.git
-        nixpkgs.legacyPackages.aarch64-darwin.curl
-        nixpkgs.legacyPackages.aarch64-darwin.ldd
-      ];
+
+      apps.${system}.update = {
+        type = "app";
+        program = toString
+          (pkgs.writeShellScript "update-script" ''
+            set -e
+            echo "Updating flake..."
+            nix flake update
+            echo "Update profile"
+            nix profile upgrade my-packages
+            echo "Update complete"
+
+          '');
+
+      };
     };
 
-  };
 }
