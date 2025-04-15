555e594244e7581bad04abda3dfa6bcb578f63dd
happy663 <tatu2425@gmail.com>
2025-04-15 09:07:41 +0900

    feat: fold settings

--- conf/.config/nvim/lua/core/settings.lua
+++ conf/.config/nvim/lua/core/settings.lua
@@ -207,3 +207,11 @@ vim.keymap.set(
   Toggle_diagnostics,
   { noremap = true, silent = true, desc = "Toggle vim diagnostics" }
 )
+
+vim.opt.foldmethod = "indent" -- インデントで折りたたみ
+vim.opt.foldlevel = 99 -- 折りたたみの初期レベルを99に設定
+
+vim.keymap.set("n", "<Tab>", "zo", { noremap = true, silent = true, desc = "Fold open" })
+vim.keymap.set("n", "<S-Tab>", "zc", { noremap = true, silent = true, desc = "Fold close" })
+vim.keymap.set("n", "<leader><Tab>", "zR", { noremap = true, silent = true, desc = "Fold open all" })
+vim.keymap.set("n", "<leader><S-Tab>", "zM", { noremap = true, silent = true, desc = "Fold close all" })
