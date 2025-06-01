18715ace45baeab06c6814f88ddf0f78db2d4fc7
happy663 <tatu2425@gmail.com>
2025-06-01 20:37:45 +0900

    update: setting

--- conf/.config/nvim/lua/plugins/colorschemas/ayu.lua
+++ conf/.config/nvim/lua/plugins/colorschemas/ayu.lua
@@ -5,16 +5,16 @@ return {
     config = function()
       require("ayu").setup({
         overrides = {
-          Normal = { bg = "None" },
+          -- Normal = { bg = "None" },
           ColorColumn = { bg = "None" },
           SignColumn = { bg = "None" },
           Folded = { bg = "None" },
           FoldColumn = { bg = "None" },
           CursorLine = { bg = "None" },
           CursorColumn = { bg = "None" },
-          WhichKeyFloat = { bg = "None" },
-          VertSplit = { bg = "None" },
-          LineNr = { fg = "#7c869c" },
+          -- WhichKeyFloat = { bg = "None" },
+          -- VertSplit = { bg = "None" },
+          -- LineNr = { fg = "#7c869c" },
         },
       })
     end,
