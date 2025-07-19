9a4ad5a57765d846dbb63d63241c1f9c997c1912
happy663 <tatu2425@gmail.com>
2025-07-19 17:06:58 +0900

    update:

--- conf/.config/nvim/lua/plugins/completion/snip.lua
+++ conf/.config/nvim/lua/plugins/completion/snip.lua
@@ -12,14 +12,73 @@ return {
       local i = ls.insert_node
       local c = ls.choice_node
 
-      -- markdown用のスニペットを追加
+      -- markdown用とOctoバッファ用のスニペットを追加
       ls.add_snippets("markdown", {
         -- 汎用コードブロック
         s("code", {
           t("```"),
-          i(1, "language"),
           t({ "", "" }),
-          i(2, "code here"),
+          i(1, "code here"),
+          t({ "", "```" }),
+        }),
+
+        -- JavaScript コードブロック
+        s("js", {
+          t({ "```javascript", "" }),
+          i(1, "// JavaScript code here"),
+          t({ "", "```" }),
+        }),
+
+        -- Python コードブロック
+        s("py", {
+          t({ "```python", "" }),
+          i(1, "# Python code here"),
+          t({ "", "```" }),
+        }),
+
+        -- Lua コードブロック
+        s("lua", {
+          t({ "```lua", "" }),
+          i(1, "-- Lua code here"),
+          t({ "", "```" }),
+        }),
+
+        -- Bash コードブロック
+        s("bash", {
+          t({ "```bash", "" }),
+          i(1, "# Bash command here"),
+          t({ "", "```" }),
+        }),
+
+        -- TypeScript コードブロック
+        s("ts", {
+          t({ "```typescript", "" }),
+          i(1, "// TypeScript code here"),
+          t({ "", "```" }),
+        }),
+
+        -- JSON コードブロック
+        s("json", {
+          t({ "```json", "" }),
+          i(1, '{"key": "value"}'),
+          t({ "", "```" }),
+        }),
+
+        -- YAML コードブロック
+        s("yaml", {
+          t({ "```yaml", "" }),
+          i(1, "key: value"),
+          t({ "", "```" }),
+        }),
+      })
+
+      -- Octoバッファ用のスニペットを追加
+      ls.add_snippets("octo", {
+        -- 汎用コードブロック
+        s("code", {
+          t("```"),
+          t({ "", "" }),
+          i(1, "code here"),
           t({ "", "```" }),
         }),
 
