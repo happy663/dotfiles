9b5c6cc626fae31f56a1e357410d1e1c3a9d5a12
happy663 <tatu2425@gmail.com>
2025-07-21 00:42:23 +0900

    feat: lazygitの終了時にウィンドウレイアウトを修復するコールバックを追加

--- conf/.config/nvim/lua/plugins/git/lazygit.lua
+++ conf/.config/nvim/lua/plugins/git/lazygit.lua
@@ -28,6 +28,40 @@ return {
           vim.api.nvim_buf_set_keymap(0, "t", "<esc>", "<esc>", opts)
         end,
       })
+
+      -- lazygit終了時のコールバック設定
+      vim.g.lazygit_on_exit_callback = function()
+        -- ウィンドウレイアウトの修復
+        vim.schedule(function()
+          -- フローティングウィンドウやターミナルバッファをクリーンアップ
+          for _, win in pairs(vim.api.nvim_list_wins()) do
+            if vim.api.nvim_win_is_valid(win) then
+              local buf = vim.api.nvim_win_get_buf(win)
+              local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+              local name = vim.api.nvim_buf_get_name(buf)
+
+              -- 不要なバッファタイプを削除
+              if ft == "lazygit" or ft == "cmp_menu" or ft == "NvimSeparator" or name == "" then
+                if vim.api.nvim_buf_is_valid(buf) and #vim.api.nvim_list_wins() > 1 then
+                  pcall(vim.api.nvim_win_close, win, true)
+                end
+              end
+            end
+          end
+
+          -- メインエディタウィンドウにフォーカス
+          for _, win in pairs(vim.api.nvim_list_wins()) do
+            if vim.api.nvim_win_is_valid(win) then
+              local buf = vim.api.nvim_win_get_buf(win)
+              local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+              if ft == "lua" or ft == "python" or ft == "javascript" or ft ~= "NvimTree" then
+                vim.api.nvim_set_current_win(win)
+                break
+              end
+            end
+          end
+        end)
+      end
     end,
   },
 }
