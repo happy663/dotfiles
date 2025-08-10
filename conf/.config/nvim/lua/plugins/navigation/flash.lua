3ab3f6305247ca82395ce9cb27457af4f5cf09b0
happy663 <tatu2425@gmail.com>
2025-08-11 00:39:46 +0900

    feat: flash.nvimの設定を更新

--- conf/.config/nvim/lua/plugins/navigation/flash.lua
+++ conf/.config/nvim/lua/plugins/navigation/flash.lua
@@ -3,11 +3,10 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     ---@type Flash.Config
-    opts = {},
-  -- stylua: ignore
+    -- stylua: ignore
     keys = {
-      -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
       { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash Jump (日本語対応)" },
       -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
