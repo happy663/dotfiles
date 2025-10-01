90359106aa32e03fc8d5a7744ac3bf8be72d7fa0
happy663 <tatu2425@gmail.com>
2025-10-02 00:41:47 +0900

    feat: Hammerspoonのモジュール読み込みを一時的に無効化

--- conf/.config/hammerspoon/init.lua
+++ conf/.config/hammerspoon/init.lua
@@ -2,7 +2,7 @@
 hs.application.enableSpotlightForNameSearches(true)
 
 -- ~/.hammerspoon/init.lua
-dofile(hs.configdir .. "/modules/ghost.lua")
+-- dofile(hs.configdir .. "/modules/ghost.lua")
 -- dofile(hs.configdir .. "/modules/test.lua")
 -- dofile(hs.configdir .. "/modules/google-docs.lua")
 -- dofile(hs.configdir .. "/modules/macskk.lua")
