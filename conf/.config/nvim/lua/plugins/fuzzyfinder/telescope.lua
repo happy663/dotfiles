1e147177ebe8d47ffe3e03802bb90d56c46da356
happy663 <tatu2425@gmail.com>
2025-10-06 10:19:15 +0900

    update: livegrep argsの設定

--- conf/.config/nvim/lua/plugins/fuzzyfinder/telescope.lua
+++ conf/.config/nvim/lua/plugins/fuzzyfinder/telescope.lua
@@ -8,6 +8,7 @@ return {
       local actions = require("telescope.actions")
       local state = require("telescope.actions.state")
       local builtin = require("telescope.builtin")
+      -- local lga_actions = require("telescope-live-grep-args.actions")
 
       -- Utility functions
       local function highlight_search_term(search_term)
@@ -153,6 +154,23 @@ return {
             max_history = 100,
             confirm_action = custom_actions.select_with_highlight(),
           },
+          -- TODO: 後で設定する
+          live_grep_args = {
+            auto_quoting = true, -- enable/disable auto-quoting
+            -- define mappings, e.g.
+            -- mappings = { -- extend mappings
+            --   i = {
+            --     ["<C-k>"] = lga_actions.quote_prompt(),
+            --     ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+            --     -- freeze the current list and start a fuzzy search in the frozen list
+            --     ["<C-space>"] = lga_actions.to_fuzzy_refine,
+            --   },
+            -- },
+            -- ... also accepts theme settings, for example:
+            -- theme = "dropdown", -- use dropdown theme
+            -- theme = { }, -- use own theme spec
+            -- layout_config = { mirror=true }, -- mirror preview pane
+          },
         },
       })
 
@@ -190,10 +208,21 @@ return {
       vim.keymap.set("n", "<leader>tn", function()
         builtin.find_files({ cwd = vim.fn.expand("%:p:h") })
       end)
+
+      -- local telescope = require("telescope")
+      -- telescope.load_extension("live_grep_args")
+      -- vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
     end,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
+    -- dependencies = {
+    --   "nvim-lua/plenary.nvim",
+    --   {
+    --     "nvim-telescope/telescope-live-grep-args.nvim",
+    --     -- This will not install any breaking changes.
+    --     -- For major updates, this must be adjusted manually.
+    --     version = "^1.0.0",
+    --   },
+    -- },
+
     keys = {
       {
         "<Leader>bg",
