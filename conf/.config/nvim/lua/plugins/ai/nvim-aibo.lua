8488d4ebd17b28b81c3acbeaddf7d80c467753ae
happy663 <tatu2425@gmail.com>
2025-10-06 10:21:25 +0900

    feat: add nvim-aibo plugin configuration

---
+++ conf/.config/nvim/lua/plugins/ai/nvim-aibo.lua
@@ -0,0 +1,13 @@
+return {
+  -- TODO: きちんと試したい
+  -- {
+  --   "lambdalisue/nvim-aibo",
+  --   -- Optional: setup can be omitted for default configuration
+  --   config = function()
+  --     require("aibo").setup({
+  --       submit_delay = 100, -- Delay in milliseconds (default: 100)
+  --       prompt_height = 10, -- Prompt window height (default: 10)
+  --     })
+  --   end,
+  -- },
+}
