e847f571030a0b2d8c5dd5b7aa01ee583d46e6f2
happy663 <tatu2425@gmail.com>
2025-11-02 00:08:47 +0900

    update: highlight-colors to include lua filetype

--- conf/.config/nvim/lua/plugins/highlight/highlight-colors.lua
+++ conf/.config/nvim/lua/plugins/highlight/highlight-colors.lua
@@ -2,8 +2,7 @@ return {
   {
     "brenoprata10/nvim-highlight-colors",
     cond = vim.g.not_in_vscode,
-    lazy = true,
-    ft = { "css", "scss", "html", "javascript", "typescript", "vue", "svelte" },
+    ft = { "css", "scss", "html", "javascript", "typescript", "vue", "svelte", "lua" },
     config = true,
   },
 }
