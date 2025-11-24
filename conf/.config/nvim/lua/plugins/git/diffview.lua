c978084f975f9f895a3b878a4aafe290f2f7a320
happy663 <tatu2425@gmail.com>
2025-11-24 23:54:11 +0900

    feat: Diffviewのキーマッピングを更新

--- conf/.config/nvim/lua/plugins/git/diffview.lua
+++ conf/.config/nvim/lua/plugins/git/diffview.lua
@@ -20,8 +20,9 @@ return {
         },
       })
       vim.keymap.set("n", "<leader>df", "<cmd>DiffviewOpen<CR>", { noremap = true, silent = true })
-      vim.keymap.set("n", "<leader>dh", "<cmd>DiffviewFileHistory --follow %<cr>", { desc = "File history" })
-      vim.keymap.set("n", "<leader>dl", "<Cmd>.DiffviewFileHistory --follow<CR>", { desc = "Line history" })
+      vim.keymap.set("n", "<leader>dvh", "<cmd>DiffviewFileHistory<CR>", { desc = "File history" })
+      vim.keymap.set("n", "<leader>dvf", "<cmd>DiffviewFileHistory --follow %<cr>", { desc = "File history %" })
+      vim.keymap.set("n", "<leader>dvl", "<Cmd>.DiffviewFileHistory --follow<CR>", { desc = "Line history" })
 
       local function get_default_branch_name()
         local res = vim.system({ "git", "rev-parse", "--verify", "main" }, { capture_output = true }):wait()
@@ -29,12 +30,12 @@ return {
       end
 
       -- Diff against local master branch
-      vim.keymap.set("n", "<leader>dm", function()
+      vim.keymap.set("n", "<leader>dvm", function()
         vim.cmd("DiffviewOpen " .. get_default_branch_name())
       end, { desc = "Diff against master" })
 
       -- Diff against remote master branch
-      vim.keymap.set("n", "<leader>dM", function()
+      vim.keymap.set("n", "<leader>dvM", function()
         vim.cmd("DiffviewOpen HEAD..origin/" .. get_default_branch_name())
       end, { desc = "Diff against origin/master" })
     end,
