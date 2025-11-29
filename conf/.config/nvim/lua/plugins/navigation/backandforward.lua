f3bacdaa0d6508b7b4bbbd8f912712e3ad4b354c
happy663 <tatu2425@gmail.com>
2025-11-30 03:58:13 +0900

    feat: Add BackAndForward.vim plugin for file-wise navigation

---
+++ conf/.config/nvim/lua/plugins/navigation/backandforward.lua
@@ -0,0 +1,8 @@
+return {
+  "Bakudankun/BackAndForward.vim",
+  event = "VeryLazy",
+  keys = {
+    { "<C-o>", "<cmd>Back<CR>", desc = "戻る(ファイル単位)", remap = true },
+    { "<C-i>", "<cmd>Forward<CR>", desc = "進む(ファイル単位)", remap = true },
+  },
+}
