f3d24529c187d27d320ca80ee37720bcea2f0bd3
happy663 <tatu2425@gmail.com>
2026-01-26 10:07:39 +0900

    update: add ask command and fix markdown-preview config

--- conf/.config/nvim/lua/plugins/note/markdown-preview.lua
+++ conf/.config/nvim/lua/plugins/note/markdown-preview.lua
@@ -1,11 +1,11 @@
 return {
   {
     "iamcco/markdown-preview.nvim",
-    cond = vim.g.not_in_vscode,
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
     end,
+    ft = { "markdown" },
   },
 }
