244acc7987b48d83c6a8bee21c88ef5afd826272
happy663 <tatu2425@gmail.com>
2026-02-11 01:57:56 +0900

    refactor: LSPハイライト設定をmason-lspconfig.luaに移動

--- conf/.config/nvim/lua/core/auto-command.lua
+++ conf/.config/nvim/lua/core/auto-command.lua
@@ -1,25 +1,6 @@
 local autocmd = vim.api.nvim_create_autocmd
-local set_hl = vim.api.nvim_set_hl
 
 vim.o.updatetime = 300
-
-local function on_cursor_hold()
-  if vim.bo.filetype ~= "NvimTree" and vim.api.nvim_get_mode().mode ~= "i" then
-    vim.lsp.buf.hover()
-  end
-end
-
-local lsp_hover_group = vim.api.nvim_create_augroup("lsp_hover", { clear = true })
--- autocmd({ "CursorHold", "CursorHoldI" }, {
---   pattern = "*",
---   group = lsp_hover_group,
---   callback = on_cursor_hold, -- ここで直接関数を指定
--- })
-
--- LSPのハイライトを設定
-set_hl(0, "LspReferenceText", { underline = true, ctermfg = 1, ctermbg = 8, fg = "#A00000", bg = "#104040" })
-set_hl(0, "LspReferenceRead", { underline = true, ctermfg = 1, ctermbg = 8, fg = "#A00000", bg = "#104040" })
-set_hl(0, "LspReferenceWrite", { underline = true, ctermfg = 1, ctermbg = 8, fg = "#A00000", bg = "#104040" })
 
 -- 自動ファイル保存
 -- markdonw以外のファイルを自動で保存する
