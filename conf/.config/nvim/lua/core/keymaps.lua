8704218c7d563ca1ef736c162787619761db5ee5
happy663 <tatu2425@gmail.com>
2026-02-10 23:45:24 +0900

    feat: Add profiling keymaps to Neovim configuration

--- conf/.config/nvim/lua/core/keymaps.lua
+++ conf/.config/nvim/lua/core/keymaps.lua
@@ -431,3 +431,18 @@ if vim.env.EDITPROMPT == "1" then
     vim.cmd('normal! "+p')
   end, { noremap = true, silent = true, desc = "Dump collected quotes" })
 end
+
+vim.keymap.set("n", "<leader>ups", function()
+  vim.cmd([[
+		:profile start /tmp/nvim-profile.log
+		:profile func *
+		:profile file *
+	]])
+end, { desc = "Profile Start" })
+
+vim.keymap.set("n", "<leader>upe", function()
+  vim.cmd([[
+		:profile stop
+		:e /tmp/nvim-profile.log
+	]])
+end, { desc = "Profile End" })
