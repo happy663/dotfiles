6c98efa97d7f65c8ec32d8375b5e1e46fb980bfa
happy663 <tatu2425@gmail.com>
2026-03-07 22:28:59 +0900

    feat: ターミナルモード表示の改善

--- conf/.config/nvim/lua/plugins/terminal/toggleterm.lua
+++ conf/.config/nvim/lua/plugins/terminal/toggleterm.lua
@@ -25,6 +25,36 @@ return {
         },
         auto_scroll = false, -- automatically scroll to the bottom on terminal output
       })
+
+      -- ターミナルモード表示: ボーダー色でInsert/Normalを区別
+      local augroup = vim.api.nvim_create_augroup("ToggleTermModes", { clear = true })
+
+      vim.api.nvim_set_hl(0, "ToggleTermInsertBorder", { fg = "#78ccc5" })
+      vim.api.nvim_set_hl(0, "ToggleTermNormalBorder", { fg = "#908caa" })
+
+      vim.api.nvim_create_autocmd("ModeChanged", {
+        group = augroup,
+        pattern = "*:t",
+        callback = function()
+          local win = vim.api.nvim_get_current_win()
+          local config = vim.api.nvim_win_get_config(win)
+          if config.relative ~= "" then
+            vim.wo.winhighlight = "FloatBorder:ToggleTermInsertBorder"
+          end
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("ModeChanged", {
+        group = augroup,
+        pattern = "*:nt",
+        callback = function()
+          local win = vim.api.nvim_get_current_win()
+          local config = vim.api.nvim_win_get_config(win)
+          if config.relative ~= "" then
+            vim.wo.winhighlight = "FloatBorder:ToggleTermNormalBorder"
+          end
+        end,
+      })
     end,
   },
 }
