45ef0de7a158cc0da647e80fbabdd1dbcede7b2a
happy663 <tatu2425@gmail.com>
2026-04-07 16:51:33 +0900

    update: codeblocktext object

--- conf/.config/nvim/lua/plugins/treesitter/treesitter-textobjects.lua
+++ conf/.config/nvim/lua/plugins/treesitter/treesitter-textobjects.lua
@@ -20,11 +20,14 @@ return {
               ["rh"] = "@assignment.rhs",
               ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
               ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+              ["ib"] = { query = "@codeblock.inner", desc = "Select inner part of a code block" },
+              ["ab"] = { query = "@codeblock.outer", desc = "Select around a code block" },
             },
             selection_modes = {
               ["@parameter.outer"] = "v", -- charwise
               ["@function.outer"] = "V", -- linewise
               ["@class.outer"] = "<c-v>", -- blockwise
+              ["@codeblock.outer"] = "V", -- linewise
             },
             include_surrounding_whitespace = true,
           },
