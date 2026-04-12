2d9073c2654cc6bc5314c1e26cffaec06d309954
happy663 <tatu2425@gmail.com>
2026-04-13 02:44:28 +0900

    update:

--- conf/.config/nvim/lua/plugins/navigation/backandforward.lua
+++ conf/.config/nvim/lua/plugins/navigation/backandforward.lua
@@ -2,10 +2,7 @@ return {
   "Bakudankun/BackAndForward.vim",
   event = "VeryLazy",
   keys = {
-    { "<A-h>", "<cmd>Back<CR>", desc = "前のファイルへ" },
-    { "<A-l>", "<cmd>Forward<CR>", desc = "次のファイルへ" },
+    { "<M-[>", "<cmd>Back<CR>", desc = "前のファイルへ" },
+    { "<M-]>", "<cmd>Forward<CR>", desc = "次のファイルへ" },
   },
 }
-
-
-
