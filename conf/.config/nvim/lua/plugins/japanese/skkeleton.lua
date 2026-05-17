218bb0d4b7edb989b177c41c660b03eda64ef891
happy663 <tatu2425@gmail.com>
2026-05-17 22:42:41 +0900

    update: add comment

--- conf/.config/nvim/lua/plugins/japanese/skkeleton.lua
+++ conf/.config/nvim/lua/plugins/japanese/skkeleton.lua
@@ -50,6 +50,9 @@ return {
             keepState = true,
             debug = false,
             registerConvertResult = true,
+            -- skkServerPortは数値リテラルで指定する (is.Numberバリデーション)。
+            -- skkServerResEncはサーバ仕様によりUTF-8固定。
+            -- 詳細: https://github.com/happy663/dotfiles/issues/255#issuecomment-4470747322
             skkServerHost = "127.0.0.1",
             skkServerPort = 1180,
             skkServerReqEnc = "euc-jp",
