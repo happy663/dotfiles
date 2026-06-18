537f2f674ff7e4f8d45d3c50694bdc391385d963
happy663 <tatu2425@gmail.com>
2026-06-19 02:15:20 +0900

    update: tversteeg/registers.nvim を happy663/registers.nvim に置き換え

--- conf/.config/nvim/lua/plugins/edit_support/registers.lua
+++ conf/.config/nvim/lua/plugins/edit_support/registers.lua
@@ -1,6 +1,8 @@
 return {
   {
-    "tversteeg/registers.nvim",
+    -- NOTE: upstream tversteeg/registers.nvim は削除済み。happy663/registers.nvim にバックアップ済み
+    -- TODO: メンテされている代替プラグインへの移行を検討
+    "happy663/registers.nvim",
     cond = vim.g.not_in_vscode,
     lazy = true,
     keys = {
