e2c4830d153f2cba0793f5592934bcb08e8fc896
happy663 <tatu2425@gmail.com>
2026-07-11 19:13:57 +0900

    feat: guh.nvim に Ug キーマップを追加
    README 推奨の "どこからでもカーソル下対象を開く" マッピング。
    non-guh バッファ (通常のコード、issue 本文、コミットメッセージなど) の
    中に転がっている PR番号 / GitHub URL / owner/repo#N を Ug で開けるようにする。
    
    lazy の keys = {} 経由なので、初回 Ug 押下で guh.nvim がロードされる。
    
    refs #298

--- conf/.config/nvim/lua/plugins/git/guh.lua
+++ conf/.config/nvim/lua/plugins/git/guh.lua
@@ -5,5 +5,9 @@ return {
     "justinmk/guh.nvim",
     lazy = true,
     cmd = { "Guh" },
+    keys = {
+      -- どこからでもカーソル下の PR番号 / URL / owner/repo#N を開く (README 推奨)
+      { "Ug", "<cmd>Guh .<cr>", desc = "Guh: open target under cursor" },
+    },
   },
 }
